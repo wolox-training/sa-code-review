bdf2b6d98a94fbc029cf0f6fb1bd8964db8d50d2
Alejandro Bezdjian <alebezdjian@gmail.com>
2018-03-01 14:04:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Some App
+
+This app serves some content and has some features for some users.
+
+Specifically, this app focuses on developers and tries to show the user all the information that devs want to see.
---
+++ spec/support/spec_helper.rb
@@ -0,0 +1,22 @@
+require 'database_cleaner'
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.file_fixture_path = Rails.root.join('spec', 'support', 'fixtures')
+  config.infer_spec_type_from_file_location!
+  config.order = 'random'
+end
+
+ActiveRecord::Migration.maintain_test_schema!
